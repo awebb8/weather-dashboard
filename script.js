54f4d8dd544a4646d1129be3396b630a439f1fab
Alkawe <alixkwebb@gmail.com>
2020-09-15 22:32:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+
+// Declare variable for search button
+var searchBtn = $("#search-button");
+var input = $("#search-input");
+// Declare variables for city information
+var temperatureEl = $("#temperature");
+var humidityEl = $("#humidity");
+var windSpeedEl = $("#wind-speed");
+var uvIndexEl = $("#uv-index");
+
+var cityName = $(".city-name-card");
+
+// WHEN I search for a city 
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+searchBtn.on("click", function(e) {
+    console.log("search button clicked");
+    e.preventDefault();
+
+    console.log(input.val());
+    var date = moment().format("MM/DD/YYYY");
+    console.log(date);
+
+    cityName.text(input.val() + "  " + date);
+
+$.ajax ({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + input.val() + "&appid=26c03499d95767da96d6275ea42c7a28",
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+
+
+
+    temperatureEl.text(response.main.temp);
+    console.log(temperatureEl);
+    humidityEl.text(response.main.humidity);
+    console.log(humidityEl);
+    var wind = windSpeedEl.text(response.wind.speed);
+    console.log(windSpeedEl);
+
+    $(temp).append(temperatureEl);
+
+});
+
+$.ajax ({
+    url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=26c03499d95767da96d6275ea42c7a28",
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+
+
+    for (i = 0; i < 5; i++) {
+
+    $(".forecast-card").text(moment().add(i, 'days').format("MM/DD/YYYY"));
+    }
+
+    var latitude = response.city.coord.lat;
+    var longitude = response.city.coord.lon;
+    console.log("lat:" + latitude + "lon: " + longitude);
+
+    $.ajax ({
+        url: "http://api.openweathermap.org/data/2.5/uvi?appid=26c03499d95767da96d6275ea42c7a28&lat=" + latitude + "&lon=" + longitude,
+        method: "GET"
+    }).then(function(uvResponse) {
+        console.log(uvResponse);
+        uvIndexEl.text(uvResponse.value);
+    });
+
+    });
+});
+
+
+
+
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast 
+});
